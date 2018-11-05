1c44d8abc6a28314cb20142b1896c8307b6f5ed1
Adibta Triantama <adibtatriantama@gmail.com>
2018-11-05 21:41:53 +0700

    Create Firebase load all methods create load all method for user, chatRoom, and message

--- app/src/main/java/com/archaic/firechaic/data/firebase/client/DatabaseClient.kt
+++ app/src/main/java/com/archaic/firechaic/data/firebase/client/DatabaseClient.kt
@@ -29,8 +29,7 @@ object DatabaseClient {
     }
 
     fun loadSingleUser(userId: String, loadItemCallback: LoadItemCallback<User>) {
-        val query: Query = getInstance()
-            .reference.child(REF_USER).child(userId)
+        val query: Query = getInstance().reference.child(REF_USER).child(userId)
         query.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 Log.d(TAG, p0.message)
@@ -47,9 +46,31 @@ object DatabaseClient {
         })
     }
 
+    fun loadAllUser(loadItemCallback: LoadItemCallback<List<User>>) {
+        val query: Query = getInstance().reference.child(REF_USER)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d(TAG, p0.message)
+                loadItemCallback.onFailed()
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val userList: MutableList<User> = mutableListOf()
+                for (childSnapshot in p0.children) {
+                    val fireUser = p0.getValue(FireUser::class.java)
+                    if (fireUser != null) {
+                        val user = ModelAdapter.userFromFireUser(fireUser)
+                        userList.add(user)
+                    }
+                }
+                if (userList.size > 0) loadItemCallback.onLoaded(userList)
+                else loadItemCallback.onFailed()
+            }
+        })
+    }
+
     fun loadSingleChatRoom(chatRoomId: String, loadItemCallback: LoadItemCallback<ChatRoom>) {
-        val query: Query = getInstance()
-            .reference.child(REF_CHATROOM).child(chatRoomId)
+        val query: Query = getInstance().reference.child(REF_CHATROOM).child(chatRoomId)
         query.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 Log.d(TAG, p0.message)
@@ -66,10 +87,34 @@ object DatabaseClient {
         })
     }
 
-    fun loadSingleMessage(messageId: String, myUserId: String, chatRoomId: String,
-                          loadMessageCallback: LoadMessageCallback) {
-        val query: Query = getInstance()
-            .reference.child(REF_MESSAGE_LOG).child(chatRoomId).child(messageId)
+    fun loadAllChatroom(loadItemCallback: LoadItemCallback<List<ChatRoom>>) {
+        val query: Query = getInstance().reference.child(REF_CHATROOM)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d(TAG, p0.message)
+                loadItemCallback.onFailed()
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val chatRoomList: MutableList<ChatRoom> = mutableListOf()
+                for (childSnapshot in p0.children) {
+                    val fireChatRoom = p0.getValue(FireChatRoom::class.java)
+                    if (fireChatRoom != null) {
+                        val chatRoom = ModelAdapter.chatRoomFromFireChatRoom(fireChatRoom)
+                        chatRoomList.add(chatRoom)
+                    }
+                }
+                if (chatRoomList.size > 0) loadItemCallback.onLoaded(chatRoomList)
+                else loadItemCallback.onFailed()
+            }
+        })
+    }
+
+    fun loadSingleMessage(
+        messageId: String, myUserId: String, chatRoomId: String,
+        loadMessageCallback: LoadMessageCallback
+    ) {
+        val query: Query = getInstance().reference.child(REF_MESSAGE_LOG).child(chatRoomId).child(messageId)
         query.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 Log.d(TAG, p0.message)
@@ -87,6 +132,32 @@ object DatabaseClient {
                         loadMessageCallback.onLoaded(message)
                     }
                 } else loadMessageCallback.onFailed()
+            }
+        })
+    }
+
+    fun loadAllMessage(myUserId: String, chatRoomId: String, loadMessageCallback: LoadMessageCallback) {
+        val query: Query = getInstance().reference.child(REF_MESSAGE_LOG).child(chatRoomId)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                Log.d(TAG, p0.message)
+                loadMessageCallback.onFailed()
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (childSnapshot in p0.children) {
+                    val fireMessage = p0.getValue(FireMessage::class.java)
+                    if (fireMessage != null) {
+                        val action = FireMessage.FireMessageAction.valueOf(fireMessage.action)
+                        if (action == FireMessage.FireMessageAction.DELETE) {
+                            loadMessageCallback.onInterrupt(action, fireMessage.targetId!!)
+                        } else {
+                            val message = ModelAdapter
+                                .messageFromFireMessage(myUserId, chatRoomId, fireMessage)
+                            loadMessageCallback.onLoaded(message)
+                        }
+                    }
+                }
             }
         })
     }
